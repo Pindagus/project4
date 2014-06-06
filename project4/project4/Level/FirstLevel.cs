d58db24acd7fed866afeff54a2c6c47dd7683771
LeonStel <leonvanderstel@hotmail.com>
2014-06-06 10:16:52 +0200

    Levels added

---
+++ project4/project4/Level/FirstLevel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace project4
+{
+    class FirstLevel : BaseLevel
+    {
+        private FirstLevelMap _firstLevelMap;
+
+        public FirstLevel(Game game)
+            : base (game)
+        {
+            //creates new map, to create new map with new structure you have to make new class in Map/Structure directory
+            //check code of the two example maps
+            _firstLevelMap = new FirstLevelMap(game);
+        }
+    }
+}
