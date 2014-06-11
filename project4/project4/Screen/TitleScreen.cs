544227857948df4ae586f2204d9503a06a8822b8
biedo123 <jeroenmonst@gmail.com>
2014-06-11 22:36:24 +0200

    changed images of the starting screen

--- project4/project4/Screen/TitleScreen.cs
+++ project4/project4/Screen/TitleScreen.cs
@@ -15,13 +15,13 @@ namespace project4
         public TitleScreen(Game game) { 
 
         //creates background    
-            Background = new ScreenElement(game, @"img\ScreenElements\background_example", new Vector2(0, 0), 0);
+            Background = new ScreenElement(game, @"img\ScreenElements\background", new Vector2(0, 0), 0);
 
         //creates Startbutton
-            StartButton = new ScreenElement(game, @"img\ScreenElements\start", new Vector2(150, 280), 0.2f);
+            StartButton = new ScreenElement(game, @"img\ScreenElements\start", new Vector2(320, 280), 0.2f);
 
         //creates ExitButton
-            ExitButton = new ScreenElement(game, @"img\ScreenElements\stop", new Vector2(150, 380), 0.2f);
+            ExitButton = new ScreenElement(game, @"img\ScreenElements\stop", new Vector2(320, 380), 0.2f);
 
         }
     }
