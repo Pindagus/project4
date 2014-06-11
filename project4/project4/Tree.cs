0da9365043ba0e5d50a0037f3782c2ba8105175e
biedo123 <jeroenmonst@gmail.com>
2014-06-11 20:46:52 +0200

    added tree class

---
+++ project4/project4/Tree.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace project4
+{
+    class Tree : GameObject
+    {
+        //private Texture2D debug;
+
+        public Tree(Game game, int X, int Y)
+            : base(game)
+        {
+            TileX = X;
+            TileY = Y;
+
+            layerDepth = ComputeDepth - 0.01;
+
+            scale = 1;
+
+            base.Initialize();
+
+            Origin = new Vector2(
+                    widthOffset,
+                    texture.Height - BaseTile.TileHeight
+                    );
+        }
+
+        protected override void LoadContent()
+        {
+            //debug boundingbox
+            //debug = new Texture2D(GraphicsDevice, 1, 1);
+            //debug.SetData(new Color[] { Color.White });
+
+            texture = Game.Content.Load<Texture2D>(@"img\GameObjects\tree");
+
+            base.LoadContent();
+        }
+
+        //delete after debugging
+        public override void Draw(GameTime gameTime)
+        {
+            //debug draw boundingbox
+            //Game1.spriteBatch.Draw(
+            //    debug,
+            //    boundingBox,
+            //    Color.Green
+            //    );
+
+            base.Draw(gameTime);
+        }
+
+
+    }
+}
